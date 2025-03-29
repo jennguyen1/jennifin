a35af04bdfafa53a3bcaab510b33af4308e7c92f
Jenny <jnnguyen2@wisc.edu>
2025-03-29 11:13:09 -0400

    mv all db update, time consuming builds to this file

--- data/etf_screener.R
+++ data/etf_screener.R
@@ -1,24 +1,27 @@
+# load settings
+source("global.R")
 
-library(tidyverse)
-source("www/util_init.R")
+# update db (<5 min - compared to 10-15min in R)
+run_db_update() 
 
-# Sys.time() # typically 10-15 min
-# e <- readr::read_csv("data/tickers_etf.csv") %>%
-#   query_ticker_data() %>%
-#   create_ta_columns()
-# s <- readr::read_csv("data/tickers_stock.csv") %>% 
-#   query_ticker_data() %>% 
-#   create_ta_columns()
-# Sys.time()
-run_db_update() # <5 min (compared to 10-15min in R)
+# breadth animation 
+animate_breadth(2020)
 
 
-# breadth animation
-animate_breadth(s)
+### update tables on anchor date changes ###
+# # price anchor 1&2
+# add_view_price_anchor("1", anchor_1)
+# add_view_price_anchor("2", anchor_2)
+# 
+# # avwap anchor 1&2
+# add_view_avwap("1", anchor_1)
+# add_view_avwap("2", anchor_2)
+# 
+# # avwap anchor ytd
+# add_view_avwap("ytd", "2025-01-01")
 
 
 ### Every Quarter - SP Companies ###
-
 # pull data from stockcharts - get sector & industry
 # now is pre-built as example scan
 # [group is SP500] OR [group is SP400] OR [group is SP600]
@@ -28,33 +31,3 @@ animate_breadth(s)
 # [group is not SP500] AND [group is not SP400] AND [group is not SP600] AND 
 # [[exchange is NYSE  ] OR [exchange is NASDAQ]] AND
 # [ market cap > 1000 ] 
-
-
-# temporary to see if this is useful
-# if keep highlight diff green
-get_history <- function(dat){
-  if(all(is.na(dat$close))) return(NULL)
-  days <- c(252, 126, 63, 21, 10)
-  high <- purrr::map_dbl(days, ~ max(tail(dat$close, .x), na.rm = TRUE))
-  low <- purrr::map_dbl(days, ~ min(tail(dat$close, .x), na.rm = TRUE))
-  
-  data.frame(days, high, low) %>% 
-    dplyr::mutate(today = tail(dat$close, 1))
-}
-
-make_high_low_table <- function(s, dt){
-  s %>% 
-    dplyr::filter(date <= as.Date(dt)) %>% 
-    tidyr::nest(data = -ticker) %>%
-    dplyr::mutate(a = suppressWarnings(purrr::map(data, get_history))) %>%
-    tidyr::unnest(a) %>%
-    dplyr::group_by(days) %>%
-    dplyr::summarise(
-      lo = sum(low == today, na.rm = TRUE),
-      hi = sum(high == today, na.rm = TRUE),
-      diff = hi - lo
-    )
-}
-
-make_high_low_table(s, today())
-# high/low table
